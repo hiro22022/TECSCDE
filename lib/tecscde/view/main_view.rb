a6dccbf4627c9c06a28570059ec378eafbae9437
Kenji Okimoto <okimoto@clear-code.com>
2019-03-06 11:50:43 +0900

    rubocop: Fix Lint/UselessAssignment
    Remove useless assignments.

--- lib/tecscde/view/main_view.rb
+++ lib/tecscde/view/main_view.rb
@@ -829,9 +829,9 @@ module TECSCDE
       #----- Cairo version -----#
       def draw_text_h_cairo(x, y, text, obj_type, alignment)
         cr = @cairo_context_target
-        cr.select_font_face(font_family = nil, # "courier", # font_family = "Times New Roman",
-                            font_slant  = Cairo::FONT_SLANT_NORMAL,
-                            font_weight = Cairo::FONT_WEIGHT_NORMAL)
+        cr.select_font_face(nil, # "courier", # font_family = "Times New Roman",
+                            Cairo::FONT_SLANT_NORMAL,
+                            Cairo::FONT_WEIGHT_NORMAL)
         cr.set_font_size(font_size(obj_type) / 1000)
         cr_te = cr.text_extents(text)
         # p "width=#{cr_te.width} x_bearing=#{cr_te.x_bearing} height=#{cr_te.height} y_bearing=#{cr_te.y_bearing}"
@@ -922,9 +922,9 @@ module TECSCDE
       #----- Cairo version -----#
       def draw_text_v_cairo(x, y, text, obj_type, alignment)
         cr = @cairo_context_target
-        cr.select_font_face(font_family = nil, # "courier", # font_family = "Times New Roman",
-                            font_slant  = Cairo::FONT_SLANT_NORMAL,
-                            font_weight = Cairo::FONT_WEIGHT_NORMAL)
+        cr.select_font_face(nil, # "courier", # font_family = "Times New Roman",
+                            Cairo::FONT_SLANT_NORMAL,
+                            Cairo::FONT_WEIGHT_NORMAL)
         cr.set_font_size(font_size(obj_type) / 1000)
         cr_te = cr.text_extents(text)
         # p "width=#{cr_te.width} x_bearing=#{cr_te.x_bearing} height=#{cr_te.height} y_bearing=#{cr_te.y_bearing}"
@@ -1074,7 +1074,6 @@ module TECSCDE
 
       def canvas_gc_set_line_width(width)
         line_attr = @canvas_gc.line_attributes
-        line_width = line_attr[0]
         line_attr[0] = width
         @canvas_gc.set_line_attributes(*line_attr)
       end
